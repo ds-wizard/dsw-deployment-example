8795fded439b1d19e4b748d8a32b098d6e7b93a1
Marek Suchánek <MarekSuchanek@users.noreply.github.com>
2022-02-24 13:18:47 +0100

    Add create-bucket script

---
+++ create-bucket.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+MINIO_NET="dsw-deployment-example_default"
+MINIO_BUCKET="engine-wizard"
+MINIO_USER="minio"
+MINIO_PASS="minioPassword"
+
+docker run --rm --net $MINIO_NET \
+  -e MINIO_BUCKET=$MINIO_BUCKET \
+  -e MINIO_USER=$MINIO_USER \
+  -e MINIO_PASS=$MINIO_PASS \
+  --entrypoint sh minio/mc -c "\
+  mc config host add dswminio http://minio:9000 minio minioPassword && \
+  mc mb dswminio/\$MINIO_BUCKET
+"
